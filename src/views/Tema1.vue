ea84a7978047e35a7168488ad928a09ef6b9d1fa
deividramirezarias <deivid.innovatec@gmail.com>
2023-04-07 19:40:44 -0500

    ajustes
    ajustes

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -92,7 +92,7 @@
 
       figure.mt-3(data-aos="zoom-in-left")
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/foWz3CqAAgw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
 
     .row.mt-5
       h4 Criterios de medición
